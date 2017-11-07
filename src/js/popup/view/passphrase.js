a819b4368dc8ed30d7893897cf4440398fd6e067
Szymon Lesisz <sz.lesisz@gmail.com>
2017-11-07 10:23:12 +0100

    disabled passphrase save password suggestion

--- src/js/popup/view/passphrase.js
+++ src/js/popup/view/passphrase.js
@@ -7,33 +7,66 @@ import { container, showView, postMessage } from './common';
 
 export const initPassphraseView = (): void => {
     showView('passphrase');
-    const input: HTMLInputElement = (container.getElementsByClassName('input')[0]: any);
-    const checkbox: HTMLElement = container.getElementsByClassName('passphrase_checkbox')[0];
+    const input: HTMLInputElement = container.getElementsByTagName('input')[0];
+    const show_passphrase: HTMLElement = container.getElementsByClassName('show_passphrase')[0];
+    const save_passphrase: HTMLElement = container.getElementsByClassName('show_passphrase')[0];
     const enter: HTMLElement = container.getElementsByClassName('submit')[0];
 
-    checkbox.addEventListener('click', () => {
-        input.type = (input.type === 'text') ? 'password' : 'text';
+    const DOT: string = '•';
+    let password: boolean = true;
+    let passValue: string = '';
+
+    const onFocusIn = (): void => {
+        input.setAttribute('type', 'password');
+        input.value = passValue;
+    }
+    const onFocusOut = (): void => {
+        passValue = input.value;
+        input.setAttribute('type', 'text');
+        input.value = passValue.replace(/./g, DOT);
+    }
+
+    input.addEventListener('focusin', onFocusIn, false);
+    input.addEventListener('focusout', onFocusOut, false);
+
+    show_passphrase.addEventListener('click', () => {
+        if (password) {
+            password = false;
+            input.setAttribute('type', 'text');
+            input.value = passValue;
+            input.removeEventListener('focusin', onFocusIn, false);
+            input.removeEventListener('focusout', onFocusOut, false);
+        } else {
+            password = true;
+            input.value = passValue.replace(/./g, DOT);
+            input.addEventListener('focusin', onFocusIn, false);
+            input.addEventListener('focusout', onFocusOut, false);
+        }
         input.focus();
     });
 
     enter.addEventListener('click', () => {
         window.removeEventListener('keydown', passKeyboardHandler);
+        input.removeEventListener('focusin', onFocusIn, false);
+        input.removeEventListener('focusout', onFocusOut, false);
+
         showView('loader');
-        postMessage(new UiMessage(UI.RECEIVE_PASSPHRASE, input.value) );
+        postMessage(new UiMessage(UI.RECEIVE_PASSPHRASE, passValue) );
     });
+
+    const passKeyboardHandler = (event: KeyboardEvent): void => {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            input.blur();
+            submit();
+        }
+    }
+
+    const submit = (): void => {
+        const button = container.getElementsByClassName('submit')[0];
+        button.click();
+    }
 
     window.addEventListener('keydown', passKeyboardHandler, false);
     input.focus();
-}
-
-const passKeyboardHandler = (event: KeyboardEvent): void => {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        submit();
-    }
-}
-
-const submit = (): void => {
-    const button = container.getElementsByClassName('submit')[0];
-    button.click();
 }
