765517493445db36b446acde02e9980bdca872b3
Szymon Lesisz <sz.lesisz@gmail.com>
2017-11-24 18:57:46 +0100

    index-lib new ui

--- src/js/constants/popup.js
+++ src/js/constants/popup.js
@@ -7,4 +7,4 @@ export const OPEN_TIMEOUT: string = 'popup_open_timeout';
 export const HANDSHAKE: string = 'popup_handshake';
 export const CLOSE: string = 'popup_close';
 export const CLOSED: string = 'popup_closed';
-export const CANCEL_POPUP_REQUEST: string = 'ui_cancel_popup_request';
+export const CANCEL_POPUP_REQUEST: string = 'ui_cancel-popup-request';
