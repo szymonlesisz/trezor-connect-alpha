37e3554d12377aceb5e34a32d2141805c58985f5
Karel Bilek <kb@karelbilek.com>
2017-11-28 17:59:21 +0100

    Eslinting Device.js, removing releaseAfterConnect

--- src/js/device/Device.js
+++ src/js/device/Device.js
@@ -4,12 +4,9 @@
 import EventEmitter from 'events';
 import semvercmp from 'semver-compare';
 import DeviceCommands from './DeviceCommands';
-import type { DefaultMessageResponse } from './DeviceCommands';
-import { resolveAfter } from '../utils/promiseUtils';
 
 import type { Features } from './trezorTypes';
 import type { Transport, TrezorDeviceInfoWithSession as DeviceDescriptor } from 'trezor-link';
-import DataManager from '../data/DataManager';
 
 import * as DEVICE from '../constants/device';
 import * as ERROR from '../constants/errors';
@@ -17,7 +14,6 @@ import { create as createDeferred } from '../utils/deferred';
 import type { Deferred } from '../utils/deferred';
 
 import Log, { init as initLog } from '../utils/debug';
-
 
 const FEATURES_LIFETIME: number = 10 * 60 * 1000; // 10 minutes
 
@@ -28,30 +24,30 @@ export type RunOptions = {
 
     // releaseAfterConnect - on the first run (called from DeviceList) release session automatically
     //                       or hold session for future use (example: waiting for device popup)
-    releaseAfterConnect?: boolean;
+    releaseAfterConnect?: boolean,
 
     // aggressive - stealing even when someone else is running things
-    aggressive?: boolean;
+    aggressive?: boolean,
     // skipFinalReload - normally, after action, features are reloaded again
     //                   because some actions modify the features
     //                   but sometimes, you don't need that and can skip that
-    skipFinalReload?: boolean;
+    skipFinalReload?: boolean,
     // waiting - if waiting and someone else holds the session, it waits until it's free
     //          and if it fails on acquire (because of more tabs acquiring simultaneously),
     //          it tries repeatedly
-    waiting?: boolean;
-    onlyOneActivity?: boolean;
+    waiting?: boolean,
+    onlyOneActivity?: boolean,
 
     // cancel popup request when we are sure that there is no need to authenticate
     // Method gets called after run() fetch new Features but before trezor-link dispatch "acquire" event
-    cancelPopupRequest?: Function;
+    cancelPopupRequest?: Function,
 }
 
 const parseRunOptions = (options?: RunOptions): RunOptions => {
     if (!options) options = {};
     options.releaseAfterConnect = typeof options.releaseAfterConnect === 'boolean' ? options.releaseAfterConnect : true;
     return options;
-}
+};
 
 /**
  *
@@ -61,8 +57,8 @@ const parseRunOptions = (options?: RunOptions): RunOptions => {
  * @extends {EventEmitter}
  */
 export default class Device extends EventEmitter {
-    transport: Transport;
-    originalDescriptor: DeviceDescriptor;
+    +transport: Transport;
+    +originalDescriptor: DeviceDescriptor;
     features: Features;
     featuresNeedsReload: boolean = false;
 
@@ -71,7 +67,6 @@ export default class Device extends EventEmitter {
 
     loaded: boolean = false;
     firstRunPromise: Deferred<boolean>;
-
 
     activitySessionID: string;
 
@@ -100,8 +95,8 @@ export default class Device extends EventEmitter {
         try {
             const device: Device = new Device(transport, descriptor);
             return device;
-        } catch(error) {
-            logger.error("Device.fromDescriptor", error);
+        } catch (error) {
+            logger.error('Device.fromDescriptor', error);
             throw error;
         }
     }
@@ -118,10 +113,10 @@ export default class Device extends EventEmitter {
         this.deferredActions[ DEVICE.ACQUIRE ] = createDeferred();
 
         const sessionID: string = await this.transport.acquire({
-                path: this.originalDescriptor.path,
-                previous: this.originalDescriptor.session,
-                checkPrevious: true,
-              });
+            path: this.originalDescriptor.path,
+            previous: this.originalDescriptor.session,
+            checkPrevious: true,
+        });
         this.activitySessionID = sessionID;
         if (this.commands) {
             this.commands.dispose();
@@ -134,7 +129,7 @@ export default class Device extends EventEmitter {
 
     async release(): Promise<void> {
         if (this.isUsedHere()) {
-            logger.debug("RELEASING");
+            logger.debug('RELEASING');
 
             if (this.commands) {
                 this.commands.dispose();
@@ -142,25 +137,24 @@ export default class Device extends EventEmitter {
 
             try {
                 await this.transport.release(this.activitySessionID);
-            } catch(err) { }
-
+            } catch (err) {
+                logger.debug('Error in release');
+                logger.debug(err);
+            }
         }
     }
-
-
 
     async run(
         fn?: () => Promise<void>,
         options?: RunOptions
     ): Promise<void> {
-
         if (this.runPromise) {
             // TODO: check if this method is called twice
             // wait or return nothing?
-            logger.debug("++++++Wait for prev")
-            //await this.runPromise.promise;
-            logger.debug("TODO: is this will be called?");
-            //throw new Error('Call in progress');
+            logger.debug('++++++Wait for prev');
+            // await this.runPromise.promise;
+            logger.debug('TODO: is this will be called?');
+            // throw new Error('Call in progress');
             throw ERROR.DEVICE_CALL_IN_PROGRESS;
         }
 
@@ -170,9 +164,8 @@ export default class Device extends EventEmitter {
         return this.runPromise.promise;
     }
 
-
     interruptionFromUser(error: Error): void {
-        logger.debug("+++++interruptionFromUser")
+        logger.debug('+++++interruptionFromUser');
         if (this.runPromise) {
             // reject inner defer
             this.runPromise.reject(error);
@@ -190,9 +183,9 @@ export default class Device extends EventEmitter {
     }
 
     interruptionFromOutside(): void {
-        logger.debug("+++++interruptionFromOutside")
+        logger.debug('+++++interruptionFromOutside');
         if (this.runPromise) {
-            this.runPromise.reject( ERROR.DEVICE_USED_ELSEWHERE );
+            this.runPromise.reject(ERROR.DEVICE_USED_ELSEWHERE);
             this.runPromise = null;
         }
 
@@ -205,17 +198,15 @@ export default class Device extends EventEmitter {
         fn?: () => Promise<X>,
         options: RunOptions
     ): Promise<any> {
-
         // acquire session
         await this.acquire();
-
 
         // update features
         await this.init();
 
         // try to cancel popup request, maybe it's not too late...
-        if(this.isAuthenticated()) {
-            this.emit( DEVICE.AUTHENTICATED );
+        if (this.isAuthenticated()) {
+            this.emit(DEVICE.AUTHENTICATED);
         }
 
         // wait for event from trezor-link
@@ -226,18 +217,13 @@ export default class Device extends EventEmitter {
             await fn();
         }
 
-        //await resolveAfter(2000, null);
+        // await resolveAfter(2000, null);
 
-        if (options.releaseAfterConnect) {
-            await this.release();
-            // wait for release event
-            if (this.deferredActions[ DEVICE.RELEASE ])
-                await this.deferredActions[ DEVICE.RELEASE ].promise;
-        }
+        await this.release();
+        // wait for release event
+        if (this.deferredActions[ DEVICE.RELEASE ]) { await this.deferredActions[ DEVICE.RELEASE ].promise; }
 
-
-        if (this.runPromise)
-            this.runPromise.resolve();
+        if (this.runPromise) { this.runPromise.resolve(); }
         this.runPromise = null;
 
         this.loaded = true;
@@ -265,7 +251,7 @@ export default class Device extends EventEmitter {
     }
 
     async getFeatures(): Promise<void> {
-        //const { message } : { message: Features } = await this.typedCall('GetFeatures', 'Features');
+        // const { message } : { message: Features } = await this.typedCall('GetFeatures', 'Features');
         const { message } : { message: Features } = await this.commands.typedCall('GetFeatures', 'Features', {});
         this.features = message;
     }
@@ -283,20 +269,20 @@ export default class Device extends EventEmitter {
     }
 
     async updateDescriptor(descriptor: DeviceDescriptor): Promise<void> {
-        logger.debug("updateDescriptor", "currentSession", this.originalDescriptor.session, "upcoming", descriptor.session, "lastUsedID", this.activitySessionID)
+        logger.debug('updateDescriptor', 'currentSession', this.originalDescriptor.session, 'upcoming', descriptor.session, 'lastUsedID', this.activitySessionID);
 
         if (descriptor.session === null) {
             // released
             if (this.originalDescriptor.session === this.activitySessionID) {
                 // by myself
-                logger.debug("RELEASED BY MYSELF")
+                logger.debug('RELEASED BY MYSELF');
                 if (this.deferredActions[ DEVICE.RELEASE ]) {
                     this.deferredActions[ DEVICE.RELEASE ].resolve();
                     delete this.deferredActions[ DEVICE.RELEASE ];
                 }
             } else {
                 // by other application
-                logger.debug("RELEASED BY OTHER APP")
+                logger.debug('RELEASED BY OTHER APP');
                 this.featuresNeedsReload = true;
             }
         } else {
@@ -304,14 +290,14 @@ export default class Device extends EventEmitter {
             // TODO: Case where listen event will dispatch before this.transport.acquire (this.acquire) return ID
             if (descriptor.session === this.activitySessionID) {
                 // by myself
-                logger.debug("ACQUIRED BY MYSELF")
+                logger.debug('ACQUIRED BY MYSELF');
                 if (this.deferredActions[ DEVICE.ACQUIRE ]) {
                     this.deferredActions[ DEVICE.ACQUIRE ].resolve();
                     // delete this.deferred[ DEVICE.ACQUIRE ];
                 }
             } else {
                 // by other application
-                logger.debug("ACQUIRED BY OTHER")
+                logger.debug('ACQUIRED BY OTHER');
                 this.interruptionFromOutside();
             }
         }
@@ -320,11 +306,11 @@ export default class Device extends EventEmitter {
 
     disconnect(): void {
         // TODO: cleanup everything
-        logger.debug("DISCONNECT CLEANUP!");
+        logger.debug('DISCONNECT CLEANUP!');
         // don't try to release
         delete this.deferredActions[ DEVICE.RELEASE ];
 
-        this.interruptionFromUser( new Error('Device disconnected') );
+        this.interruptionFromUser(new Error('Device disconnected'));
 
         this.runPromise = null;
     }
@@ -360,7 +346,6 @@ export default class Device extends EventEmitter {
         throw new Error('Device does not support given coin type');
     }
 
-
     isUsed(): boolean {
         return this.originalDescriptor.session != null;
     }
@@ -374,7 +359,7 @@ export default class Device extends EventEmitter {
     }
 
     isRunning(): boolean {
-        return (this.runPromise) ? true : false;
+        return !!(this.runPromise);
     }
 
     isLoaded(): boolean {
@@ -390,13 +375,13 @@ export default class Device extends EventEmitter {
     }
 
     isAuthenticated(): boolean {
-        if (this.isUnacquired() || this.isUsedElsewhere() || this.featuresNeedsReload ) return false;
+        if (this.isUnacquired() || this.isUsedElsewhere() || this.featuresNeedsReload) return false;
         if (new Date().getTime() - this.featuresTimestamp > FEATURES_LIFETIME) return false;
 
         const pin: boolean = this.features.pin_protection ? this.features.pin_cached : true;
         let pass: boolean = this.features.passphrase_protection ? this.features.passphrase_cached : true;
         if (typeof this.cachedPassphrase === 'string') pass = true;
-        logger.debug("isAuthenticated", pin, pass, this.cachedPassphrase)
+        logger.debug('isAuthenticated', pin, pass, this.cachedPassphrase);
         return (pin && pass);
     }
 
@@ -416,32 +401,31 @@ export default class Device extends EventEmitter {
 
     // simplified object to pass via postMessage
     toMessageObject(): DeviceDescription {
-        const defaultLabel: string = "My TREZOR";
+        const defaultLabel: string = 'My TREZOR';
         if (this.isUnacquired()) {
             return {
                 path: this.originalDescriptor.path,
                 label: defaultLabel,
                 isUsedElsewhere: this.isUsedElsewhere(),
                 featuresNeedsReload: this.featuresNeedsReload,
-                unacquired: true
-            }
+                unacquired: true,
+            };
         } else {
-            let label = this.features.label !== "" ? this.features.label : defaultLabel;
+            const label = this.features.label !== '' ? this.features.label : defaultLabel;
             return {
                 path: this.originalDescriptor.path,
                 label: label,
                 isUsedElsewhere: this.isUsedElsewhere(),
-                featuresNeedsReload: this.featuresNeedsReload
-            }
+                featuresNeedsReload: this.featuresNeedsReload,
+            };
         }
     }
 }
 
-
 export type DeviceDescription = {
-    path: string;
-    label: string;
-    isUsedElsewhere: boolean;
-    featuresNeedsReload: boolean;
-    unacquired?: boolean;
+    path: string,
+    label: string,
+    isUsedElsewhere: boolean,
+    featuresNeedsReload: boolean,
+    unacquired?: boolean,
 }
