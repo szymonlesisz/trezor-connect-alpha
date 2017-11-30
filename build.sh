0c8b6064379d8d25d3d2e6c179848349b215255a
Szymon Lesisz <sz.lesisz@gmail.com>
2017-11-30 18:54:01 +0100

    build scriot (incomplete)

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@ DIST="dist"
 
 printf "\n-- DEPLOY START -----------------------\n"
 
-yarn run build
+#yarn run build
 
 printf "\n-- COPYING FILES ----------------------\n"
 
 cd DIST
+cp js/trezor-connect.*.js trezor-connect.js
+cp js/trezor-library.*.js trezor-library.js
 rsync -avz --delete -e ssh . admin@dev.sldev.cz:~/experiments/www
 cd ../
 
