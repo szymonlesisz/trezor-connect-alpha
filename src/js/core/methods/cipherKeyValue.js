f3d5ced50ec9ff6439517737bcb7f26b43b147bb
Szymon Lesisz <sz.lesisz@gmail.com>
2018-03-01 18:58:03 +0100

    cipherKeyValue params validation

--- src/js/core/methods/cipherKeyValue.js
+++ src/js/core/methods/cipherKeyValue.js
@@ -7,7 +7,7 @@ import type { MethodParams, MethodCallbacks } from './parameters';
 
 const method = async (params: MethodParams, callbacks: MethodCallbacks): Promise<Object> => {
     const input: Object = params.input;
-    const node = await callbacks.device.getCommands().cipherKeyValue(input.path, input.key, input.value, input.encrypt, input.ask_on_encrypt, input.ask_on_decrypt);
+    const node = await callbacks.device.getCommands().cipherKeyValue(input.path, input.key, input.value, input.encrypt, input.ask_on_encrypt, input.ask_on_decrypt, input.iv);
     return {
         resp: node
     };
@@ -28,6 +28,43 @@ const params = (raw: Object): MethodParams => {
     const permissions: Array<string> = checkPermissions([]);
     const requiredFirmware: string = '1.5.0';
 
+    if (!raw.hasOwnProperty('path')) {
+        throw new Error('Parameter "path" is missing');
+    } else {
+        raw.path = validatePath(raw.path);
+    }
+
+    if (raw.hasOwnProperty('key') && typeof raw.key !== 'string') {
+        throw new Error('Parameter "key" has invalid type. String expected.');
+    }
+
+    if (raw.hasOwnProperty('value') && typeof raw.value !== 'string') {
+        throw new Error('Parameter "value" has invalid type. String expected.');
+    }
+
+    if (raw.hasOwnProperty('askOnEncrypt') && typeof raw.askOnDecrypt !== 'boolean') {
+        throw new Error('Parameter "askOnEncrypt" has invalid type. Boolean expected.');
+    }
+
+    if (raw.hasOwnProperty('askOnDecrypt') && typeof raw.askOnDecrypt !== 'boolean') {
+        throw new Error('Parameter "askOnDecrypt" has invalid type. Boolean expected.');
+    }
+
+    if (raw.hasOwnProperty('useEmptyPassphrase') && typeof raw.useEmptyPassphrase !== 'boolean') {
+        throw new Error('Parameter "useEmptyPassphrase" has invalid type. Boolean expected.');
+    }
+
+    if (raw.hasOwnProperty('iv') && typeof raw.iv !== 'string') {
+        throw new Error('Parameter "iv" has invalid type. String expected.');
+    }
+
+    const allowed = ['id', 'method', 'device', 'useEmptyPassphrase', 'path', 'key', 'value', 'encrypt', 'askOnEncrypt', 'askOnDecrypt', 'iv'];
+    for (const [key, value] of Object.entries(raw)) {
+        if (allowed.indexOf(key) < 0) {
+            console.warn(`Unknown param "${key}"`);
+        }
+    }
+
     return {
         responseID: raw.id,
         name: 'cipherKeyValue',
@@ -37,13 +74,15 @@ const params = (raw: Object): MethodParams => {
         requiredPermissions: permissions,
         confirmation: null,
         method,
+        useEmptyPassphrase: raw.useEmptyPassphrase,
         input: {
-            path: validatePath(raw.path),
+            path: raw.path,
             key: raw.key,
             value: raw.value,
             encrypt: raw.encrypt,
             ask_on_encrypt: raw.askOnEncrypt,
             ask_on_decrypt: raw.askOnDecrypt,
+            iv: raw.iv
         },
     };
 };
